012a12eb8d71bc7fa926069306b0eb3fd6b2195a
shimura <shimura21ngs@gmail.com>
2021-04-10 11:17:26 +0900

    complete(not responsive)

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$('.reputation-wrapper').slick({
+			infinite: true,
+			arrow: true,
+			dots: true,
+	});
+	$('.beforbegin-contents-wrapper').slick({
+		arrows: true,
+		dots: true,
+		variableWidth: true,
+		infinete: false,
+	});
+});
